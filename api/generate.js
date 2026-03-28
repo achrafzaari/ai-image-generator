e24732108f94270037fffb2daacac936a7a05f19
achrafzaari <132367280+achrafzaari@users.noreply.github.com>
2026-03-28 23:09:08 +0100

    Update API endpoint for stable diffusion model

--- api/generate.js
+++ api/generate.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-3.5-large',
+      'https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0',
       {
         method: 'POST',
         headers: {
