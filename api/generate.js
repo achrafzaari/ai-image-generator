93ad48e8cbedfc6e5c18aa328505f00a18585c33
achrafzaari <132367280+achrafzaari@users.noreply.github.com>
2026-03-28 23:06:21 +0100

    Update API endpoint for model inference

--- api/generate.js
+++ api/generate.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev',
+      'https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev',
       {
         method: 'POST',
         headers: {
